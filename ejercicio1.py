276789c8d7f1d4d0626c83e4e667138172a51687
Lily Perera <lilycapetillo86@gmail.com>
2022-06-01 13:15:24 -0400

    Realizando el primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,27 @@
+'''
+Crear una variable que "numero1" que tenga el valor 5
+Indicar de que tipo es la variable 'numero1'
+
+Crear una variable "numero2" que tenga el valor 6.5
+Indicar de que tipo es la variable 'numero2'
+
+Crear otra variable "numero3" que tenga el valor 7
+Indicar de que tipo es la variable 'numero3'
+
+Crear otra variable "suma" que tenga el valor de la suma de los tres números anteriores (numero1, numero2, numero3)
+Mostrar el resultado del la variable "suma"
+Indicar de que tipo es la variable  "suma"
+'''
+
+numero1 = 5
+type(numero1)
+numero2 = 6.5
+type(numero2)
+numero3 = 7
+type(numero3)
+
+suma = numero1 + numero2 + numero3
+print("El valor de la suma de los 3 números es {}".format(suma))
+print(f"El valor de la suma de los 3 números es {suma}")
+
+
