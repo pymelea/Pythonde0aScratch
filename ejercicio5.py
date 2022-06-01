b090ce84b0a56612c0f05eb27addf7cae9068778
Lily Perera <lilycapetillo86@gmail.com>
2022-06-01 17:02:47 -0400

    Realizado el ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,11 @@
+'''
+Crear una variable "numero1" que contiene el valor 5
+Crear una variable "numero2" que contiene el valor 8
+Crear una variable "suma" que contiene la suma de los numero anteriores
+Imprimir por pantalla el resultado, utilizando la funcion "print"
+'''
+
+numero1 = 5
+numero2 = 8
+suma = numero1 + numero2
+print(" El resultado de sumar {} mas {} es {}".format(numero1, numero2, suma))
