a5872f57b27ddd13b72b13baed27efdeded32e2f
Lily Perera <lilycapetillo86@gmail.com>
2022-06-01 13:16:59 -0400

    Realizando el ejercicio 2.

---
+++ ejercicio2.py
@@ -0,0 +1,17 @@
+'''
+1- Crear la variable "numero1" con el valor 5.
+2- Crear la variable "numero2" con el valor 3.
+3- Crear la variable "suma" con el valor de los numeros numero1 y numero2
+4- Convetir esta variable "suma" en una cadena de caracteres y llamarla  " strSuma"
+5- Crear una variable "resultado"  que concatene el texto "El resultado es" y la variable " strSuma"
+'''
+
+numero1 = 5
+numero2 = 3
+
+suma = numero1 + numero2
+
+strSuma = str(suma)
+
+resultado = "El resultado es " + strSuma
+print(resultado)
