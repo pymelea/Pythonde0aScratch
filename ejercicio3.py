9b737cf55615219a6c7d1938822781c4f3b69a87
Lily Perera <lilycapetillo86@gmail.com>
2022-06-01 15:41:40 -0400

    Realizando el ejercicio 3.

---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+'''
+Crea una variable  "cadena" que contiene el valor "Esto es un texto de ejemplo"
+Segun la posicion de cada letra en la cadena, cacular que valores (x,y) hay que poner para seleccionar solo
+la palabra "texto"
+cadena[x:y] = "texto"
+
+'''
+
+cadena = "Esto es un texto de ejemplo"
+
+print(cadena[11:16])
