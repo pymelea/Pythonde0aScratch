53e8c6551e541173821d25362dee8cc65dee54e4
Lily Perera <lilycapetillo86@gmail.com>
2022-06-02 16:50:38 -0400

    Realizando el ejercicio 10

---
+++ ejercicio10.py
@@ -0,0 +1,12 @@
+'''
+Dada la siguiente lista = [1,2,5,25,33,56,75,21,56,89,43,13,62,24]'
+Mostrar mediante el método "print" y el operador "in", si el número 21 está en la lista
+'''
+
+lista = [1,2,5,25,33,56,75,89,43,13,62,24]
+
+for i in lista:
+    if i==  21:
+        print("El número 21 está en la lista")
+    else:
+        print ("El número 21 no está en la lista")
