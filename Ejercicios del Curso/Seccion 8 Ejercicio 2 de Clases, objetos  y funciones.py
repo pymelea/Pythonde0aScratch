a5eacee5c0b2d1abf4be8e063dae734430ce5e5b
Lily Perera <lilycapetillo86@gmail.com>
2022-06-06 16:35:42 -0400

    Realizando ejercicio 2 de a seccion 8 de Clases, objetos y funciones

---
+++ Ejercicios del Curso/Seccion 8 Ejercicio 2 de Clases, objetos  y funciones.py
@@ -0,0 +1,7 @@
+'''
+Crear una función lambda que calcule la media de 3 notas
+'''
+
+nota_media = lambda nota1, nota2, nota3 : (nota1 + nota2 + nota3) / 3
+nota_media = nota_media(5,4,3)
+print(nota_media)
